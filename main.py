cf39b119fa7d28a81d7843c087ad2e4ce0a8f4ec
FelipeMAssis <felipemassis98@gmail.com>
2024-09-02 17:10:48 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,27 @@
 import numpy as np
+from fem.nodes import Node
+from fem.elements import Element
+from fem.material import Material
+from fem.properties import Property
+
+nodes = [
+    Node(0, [0,0]),
+    Node(1, [1200,300]),
+    Node(2, [2400, 0]),
+    Node(3, [1200, 800])
+]
+
+alluminum = Material(0, 80000)
+
+rod = Property(0, alluminum, 100)
+
+elements = [
+    Element(0,[nodes[0], nodes[1]], property=rod),
+    Element(1,[nodes[1], nodes[2]], property=rod),
+    Element(2,[nodes[0], nodes[3]], property=rod),
+    Element(3,[nodes[1], nodes[3]], property=rod),
+    Element(4,[nodes[3], nodes[2]], property=rod)
+]
+
+
 
